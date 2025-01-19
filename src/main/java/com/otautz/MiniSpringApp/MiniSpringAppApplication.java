c981af92f0f9de2e09d0c15f6a0660eb9e2b4725
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2025-01-19 17:45:35 +0100

    initialize Spring

---
+++ src/main/java/com/otautz/MiniSpringApp/MiniSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.otautz.MiniSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otautz/MiniSpringApp/MiniSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otautz.MiniSpringApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
